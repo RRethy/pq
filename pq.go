de77fac26917415b75ce8ee9b5706153e0007bb2
Adam P. Regasz-Rethy <rethy.spud@gmail.com>
2020-06-03 15:36:01 -0400

    Finish crude implementation

--- pq.go
+++ pq.go
@@ -1,57 +1,115 @@
 package pq
 
+import (
+	"fmt"
+	"math"
+)
+
 // Evaluable TODO
 type Evaluable interface {
-	value() int
+	Value() int
 }
 
-// MinPriorityQueue TODO
-type MinPriorityQueue struct {
+// PQ TODO
+type PQ struct {
 	members []Evaluable
 }
 
+func (pq *PQ) String() string {
+	var mapped []int
+	for _, e := range pq.members {
+		mapped = append(mapped, e.Value())
+	}
+	return fmt.Sprintf("%v", mapped)
+}
+
 // Push TODO
-func (pq *MinPriorityQueue) Push(e Evaluable) {
+func (pq *PQ) Push(e Evaluable) {
 	pq.members = append(pq.members, e)
-	pq.fixUp()
+	pq.fixUp(pq.Len() - 1)
 }
 
 // Pop TODO
-func (pq *MinPriorityQueue) Pop() Evaluable {
-	if pq.length() == 0 {
+func (pq *PQ) Pop() Evaluable {
+	if pq.Len() == 0 {
 		return nil
 	}
-	retval := pq.members[pq.length()-1]
-	pq.members = pq.members[:pq.length()-1]
-	if pq.length() > 0 {
+	pq.swap(0, pq.Len()-1)
+	retval := pq.members[pq.Len()-1]
+	pq.members = pq.members[:pq.Len()-1]
+	if pq.Len() > 0 {
 		pq.fixDown(0)
 	}
 	return retval
 }
 
 // Min TODO
-func (pq *MinPriorityQueue) Min() Evaluable {
-	if pq.length() == 0 {
+func (pq *PQ) Min() Evaluable {
+	if pq.Len() == 0 {
 		return nil
 	}
 	return pq.members[0]
 }
 
-func (pq *MinPriorityQueue) fixUp(i int) Evaluable {
-
-}
-
-func (pq *MinPriorityQueue) fixDown(i int) Evaluable {
-
-}
-
-func (pq *MinPriorityQueue) root() Evaluable {
-	if pq.length() == 0 {
-		return nil
-	}
-	return pq.members[0]
-}
-
-func (pq *MinPriorityQueue) length() int {
+// Len TODO
+func (pq *PQ) Len() int {
 	return len(pq.members)
+}
+
+func (pq *PQ) fixUp(i int) {
+	for pq.parent(i) != -1 && pq.valueAt(pq.parent(i)) > pq.valueAt(i) {
+		pq.swap(pq.parent(i), i)
+		i = pq.parent(i)
+	}
+}
+
+func (pq *PQ) fixDown(i int) {
+	for !pq.leaf(i) {
+		minChildIndex := pq.indexMinChild(i)
+		if pq.valueAt(i) < pq.valueAt(minChildIndex) {
+			break
+		}
+		pq.swap(i, minChildIndex)
+		i = minChildIndex
+	}
+}
+
+func (pq *PQ) root() Evaluable {
+	if pq.Len() == 0 {
+		return nil
+	}
+	return pq.members[0]
+}
+
+func (pq *PQ) leaf(i int) bool {
+	return 2*i+1 >= pq.Len()
+}
+
+// TODO return an error instead of -1 if i is a leaf
+func (pq *PQ) indexMinChild(i int) int {
+	if pq.Len() > 2*i+1 {
+		if pq.Len() > 2*i+2 && pq.valueAt(2*i+1) > pq.valueAt(2*i+2) {
+			return 2*i + 2
+		}
+		return 2*i + 1
+	}
+	return -1 // i is a leaf
+}
+
+func (pq *PQ) valueAt(i int) int {
+	if i >= pq.Len() {
+		return math.MaxInt32
+	}
+	return pq.members[i].Value()
+}
+
+func (pq *PQ) swap(i, j int) {
+	pq.members[i], pq.members[j] = pq.members[j], pq.members[i]
+}
+
+func (pq *PQ) parent(i int) int {
+	if i == 0 {
+		return -1
+	}
+	return (i - 1) / 2
 }
--- pq_test.go
+++ pq_test.go
@@ -4,5 +4,53 @@ import (
 	"testing"
 )
 
+type E struct {
+	val int
+}
+
+func (e *E) Value() int {
+	return e.val
+}
+
 func TestPQ(t *testing.T) {
+	input := []*E{
+		&E{10},
+		&E{5},
+		&E{2},
+		&E{8},
+		&E{9},
+		&E{7},
+		&E{6},
+		&E{1},
+		&E{3},
+		&E{4},
+	}
+	pq := new(PQ)
+	for _, e := range input {
+		pq.Push(e)
+	}
+	prev := -1
+	var poppedOrder []*E
+	for pq.Len() > 0 {
+		popped := pq.Pop()
+		poppedOrder = append(poppedOrder, popped.(*E))
+		if popped.Value() < prev {
+			t.Errorf("pq: %v | popped: %d | prev: %d\n", pq, popped, prev)
+			break
+		}
+		prev = popped.Value()
+	}
+
+	for _, e1 := range poppedOrder {
+		found := false
+		for _, e2 := range input {
+			if e1 == e2 {
+				found = true
+				break
+			}
+		}
+		if !found {
+			t.Errorf("Couldn't find match for *E with value %d\n", e1.Value())
+		}
+	}
 }
